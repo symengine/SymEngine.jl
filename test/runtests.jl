c7b991aed7d6f9e81563deff63682b581f2a9619
Isuru Fernando <isuruf@gmail.com>
2015-07-15 20:32:29 +0530

    Added tests and refactor

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,28 @@
-using SymEngine
+using Base.Test
+import SymEngine
 
-x = basic_symbol("x")
-y = basic_symbol("y")
+x = SymEngine.symbol("x")
+y = SymEngine.symbol("y")
 
-a = x^2 + 2*x -x*y*5
-b = basic_diff(a, x)
-b = abs(b)
+a = x^2 + x/2 - x*y*5
+b = SymEngine.diff(a, x)
+@test b == 2*x + Rational(1, 2) - 5*y
+
+c = x + Rational(1, 5)
+c = SymEngine.expand(c * 5)
+@test c == 5*x + 1
+
+c = x ^ 5
+@test SymEngine.diff(c, x) == 5 * x ^ 4
+
+c = x ^ y
+@test c != y^x
+
+c = SymEngine.Basic(-5)
+@test abs(c) == 5
+@test abs(c) != 4
 
 show(a)
+println()
 show(b)
+println()
