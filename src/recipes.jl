e2a37e23cf861a08e032fc0117260d853ded2075
jverzani <jverzani@gmail.com>
2016-07-08 16:35:13 -0400

    fix copy-and-paste error

--- src/recipes.jl
+++ src/recipes.jl
@@ -2,4 +2,4 @@ using RecipesBase
 
 ## Plug into RecipesBase so plots of expressions treated like plots of functions
 @recipe f{T<:Basic}(::Type{T}, v::T) = lambdify(v)
-@recipe f{T<:Basic}(::Type{T}, v::T) = lambdify(v)
+@recipe f{S<:AbstractVector{Basic}}(::Type{S}, ss::S) = Function[lambdify(s) for s in ss]
