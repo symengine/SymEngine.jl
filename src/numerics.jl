d9a927665248e158f7a0d763ac6225afdc84d465
Isuru Fernando <isuruf@gmail.com>
2016-10-07 12:43:55 +0530

    evalf, RealMPFR, ComplexMPC

--- src/numerics.jl
+++ src/numerics.jl
@@ -4,11 +4,6 @@
 Convert a SymEngine numeric value into a number
 
 """
-N(b::Basic) = N(BasicType(b))
-N(b::BasicNumber) = eval(parse(toString(b)))  ## HACKY
-N(b::BasicType{Val{:Rational}}) = eval(parse(replace(toString(b), "/", "//")))
-N(b::BasicType{Val{:Complex}}) =  eval(parse(replace(toString(b), "I", "im")))
-
 function convert(::Type{BigInt}, b::BasicType{Val{:Integer}})
     a = BigInt()
     c = Basic(b)
@@ -52,8 +47,29 @@ function imag(b::BasicType{Val{:Complex}})
     return a
 end
 
+function real(b::BasicType{Val{:ComplexMPC}})
+    c = Basic(b)
+    a = Basic()
+    ccall((:complex_mpc_real_part, libsymengine), Void, (Ptr{Basic}, Ptr{Basic}), &a, &c)
+    return a
+end
+
+function imag(b::BasicType{Val{:ComplexMPC}})
+    c = Basic(b)
+    a = Basic()
+    ccall((:complex_mpc_imaginary_part, libsymengine), Void, (Ptr{Basic}, Ptr{Basic}), &a, &c)
+    return a
+end
+
 function convert(::Type{Complex{Cdouble}}, b::BasicType{Val{:ComplexDouble}})
-    return convert(Cdouble, real(b)) + im * convert(Cdouble, imag(b))
+    return complex(convert(Cdouble, real(b)), convert(Cdouble, imag(b)))
+end
+
+function convert(::Type{BigFloat}, b::BasicType{Val{:RealMPFR}})
+    c = Basic(b)
+    a = BigFloat()
+    ccall((:real_mpfr_get, libsymengine), Void, (Ptr{BigFloat}, Ptr{Basic}), &a, &c)
+    return a
 end
 
 function N(b::BasicType{Val{:Integer}})
@@ -65,8 +81,8 @@ function N(b::BasicType{Val{:Integer}})
     else
         u = unsafe_load(a.d, 1)
         if u <= typemax(Int64)
-            return Int64(u) * signof(a)
-        else if sign(a) == 1
+            return Int64(u) * sign(a)
+        elseif sign(a) == 1
             return u
         else
             return a
@@ -74,7 +90,12 @@ function N(b::BasicType{Val{:Integer}})
     end
 end
 
+N(b::Basic) = N(BasicType(b))
 N(b::BasicType{Val{:RealDouble}}) = convert(Cdouble, b)
+# TODO: Implement rational_get_mpq in cwrapper.h
+N(b::BasicType{Val{:Rational}}) = eval(parse(replace(toString(b), "/", "//")))
+N(b::BasicType{Val{:RealMPFR}}) = convert(BigFloat, b)
+N(b::BasicComplexNumber) = complex(N(real(b)), N(imag(b)))
 
 ## function N(b::BasicType{Val{:Rational}})
 ##     println("XXX")
@@ -94,5 +115,12 @@ N(a::Integer) = a
 N(a::Rational) = a
 N(a::Complex) = a
 
-convert(::Type{Real}, b::Basic) = convert(Real, N(b))
-convert{T}(::Type{Complex{T}}, b::Basic) = convert(Complex{T}, N(b))
+function evalf(b::Basic, bits::Integer=53, real::Bool=false)
+    c = Basic()
+    status = ccall((:basic_evalf, libsymengine), Cint, (Ptr{Basic}, Ptr{Basic}, Culong, Cint), &c, &b, Culong(bits), Int(real))
+    if status == 0
+        return c
+    else
+        throw(ArgumentError("libsymengine has to be compiled with MPFR and MPC for eval with precision greater than 53."))
+    end
+end
