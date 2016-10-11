5e005292e30b5b7a7795a0fd2c89a8ffc8ff7bbb
Isuru Fernando <isuruf@gmail.com>
2016-10-11 09:04:40 +0530

    Use conda envs

--- deps/build.jl
+++ deps/build.jl
@@ -4,14 +4,11 @@ using Conda
 @BinDeps.setup
 
 libsymengine = library_dependency("libsymengine", aliases=["libsymengine", "symengine"])
+env = Symbol(abspath(dirname(@__FILE__), "usr"))
 
-Conda.add_channel("conda-forge")
-Conda.add_channel("symengine")
-if is_windows()
-    # SymEngine can only be installed to a python 3.5 environment
-    Conda.add("python=3.5")
-end
-provides(Conda.Manager, "symengine==0.2.0", [libsymengine])
+Conda.add_channel("conda-forge", env)
+Conda.add_channel("symengine", env)
+provides(Conda.EnvManager{env}, "symengine==0.2.0", [libsymengine])
 
 @BinDeps.install Dict([(:libsymengine, :libsymengine)])
 
