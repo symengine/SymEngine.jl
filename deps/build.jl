929965e8d28f1b3d501318681b15b42b47fcc51d
Isuru Fernando <isuruf@gmail.com>
2016-10-23 23:59:55 +0530

    install python 3.5 on windows

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,12 @@ env = Symbol(abspath(dirname(@__FILE__), "usr"))
 
 Conda.add_channel("conda-forge", env)
 Conda.add_channel("symengine", env)
+
+if is_windows()
+    # SymEngine can only be installed to a python 3.5 environment
+    Conda.add("python=3.5", env)
+end
+
 provides(Conda.EnvManager{env}, "symengine==0.2.0", [libsymengine])
 
 @BinDeps.install Dict([(:libsymengine, :libsymengine)])
