e351d57d1a077780994c715dd2cca908559bfb86
Isuru Fernando <isuruf@gmail.com>
2017-01-11 22:59:29 +0530

    Install Conda only if libsymengine.so not found in SystemPaths

--- deps/build.jl
+++ deps/build.jl
@@ -16,9 +16,18 @@ if is_windows()
     provides(Binaries, URI(url), libsymengine, unpacked_dir="$path/bin")
 else
     env = Symbol(abspath(dirname(@__FILE__), "usr"))
-    Conda.add_channel("conda-forge", env)
-    Conda.add_channel("symengine", env)
-    provides(Conda.EnvManager{env}, "symengine==0.2.0", [libsymengine])
+    EnvManagerType = Conda.EnvManager{env}
+    # Conda's method will install miniconda to check that a package exists.
+    # This will indicate to BinDeps that the packages for this env exists unconditionally.
+    BinDeps.package_available(m::EnvManagerType) = true
+    # Adding the channel to conda will install miniconda even if conda is not the chosen LibraryProvider
+    # Override the command for install in this env so that channels are added.
+    function BinDeps.generate_steps(dep::BinDeps.LibraryDependency, manager::EnvManagerType, opts)
+        Conda.add_channel("conda-forge", env)
+        Conda.add_channel("symengine", env)
+        Conda.add("$(manager.packages[1])", env)
+    end
+    provides(EnvManagerType, "symengine==0.2.0", [libsymengine])
 end
 
 @BinDeps.install Dict([(:libsymengine, :libsymengine)])
