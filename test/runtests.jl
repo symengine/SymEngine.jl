b119630810d091cfaf76c4a3af4959479349956b
Isuru Fernando <isuruf@gmail.com>
2017-03-23 10:59:11 +0530

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -169,6 +169,7 @@ t = BigFloat(1.23)
 # Check that constructing Basic from Expr works
 @vars x y
 @test Basic(:(-2*x)) == -2*x
-@test Basic(:(3*x*y)) == 3*x*y
+@test Basic(:(-3*x*y)) == -3*x*y
+@test Basic(:((x-y)*-3)) == (x-y)*(-3)
 @test Basic(:(-y)) == -y
 @test Basic(:(-2*(x-2*y))) == -2*(x-2*y)
