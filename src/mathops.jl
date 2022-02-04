eae205b1b17c7dd6e3750a04e74a30f913e0b0f5
Zgoda Iu.N <YZgoda@lan.spbgasu.ru>
2022-02-04 13:34:55 +0300

    added product of n terms

--- src/mathops.jl
+++ src/mathops.jl
@@ -67,6 +67,8 @@ end
 *(b1, b2::Basic, b3, bs...) = *(Basic(b1), Basic(b2), Basic(b3), bs...)
 *(b1, b2, b3::Basic, bs...) = *(Basic(b1), Basic(b2), Basic(b3), bs...)
 
+
+
 ## ## constants
 Base.zero(x::Basic) = Basic(0)
 Base.zero(::Type{T}) where {T<:Basic} = Basic(0)
