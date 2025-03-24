5dff1ba4548fb74888b374db91d0c8d4c984f80c
jverzani <jverzani@gmail.com>
2025-03-24 09:55:57 -0400

    allocations needs v1.9

--- test/runtests.jl
+++ test/runtests.jl
@@ -355,20 +355,23 @@ end
 	@test deserialized == data
 end
 
-@vars x y
-a = Basic()
-@testset "non-allocating(ish) methods" begin
-    sin(x), cos(x), abs(x)
-    x^x, x + x, x*x, x-x, x/x # warm up
+if VERSION >= v"1.9.0"
 
-    @test (@allocations SymEngine.sin!(a,x)) == 0
-    @test (@allocations SymEngine.cos!(a,x)) == 0
-    @test (@allocations SymEngine.pow!(a,x,x)) == 0
+    @vars x y
+    a = Basic()
+    @testset "non-allocating(ish) methods" begin
+        sin(x), cos(x), abs(x)
+        x^x, x + x, x*x, x-x, x/x # warm up
 
-    # still allocates 1 (or 2)
-    @test (@allocations SymEngine.add!(a,x,y)) < (@allocations x+y)
-    @test (@allocations SymEngine.sub!(a,x,y)) < (@allocations x-y)
-    @test (@allocations SymEngine.mul!(a,x,y)) < (@allocations x*y)
-    @test (@allocations SymEngine.div!(a,x,y)) < (@allocations x/y)
-    @test (@allocations SymEngine.abs2!(a,x)) < (@allocations abs2(x))
+        @test (@allocations SymEngine.sin!(a,x)) == 0
+        @test (@allocations SymEngine.cos!(a,x)) == 0
+        @test (@allocations SymEngine.pow!(a,x,x)) == 0
+
+        # still allocates 1 (or 2)
+        @test (@allocations SymEngine.add!(a,x,y)) < (@allocations x+y)
+        @test (@allocations SymEngine.sub!(a,x,y)) < (@allocations x-y)
+        @test (@allocations SymEngine.mul!(a,x,y)) < (@allocations x*y)
+        @test (@allocations SymEngine.div!(a,x,y)) < (@allocations x/y)
+        @test (@allocations SymEngine.abs2!(a,x)) < (@allocations abs2(x))
+    end
 end
